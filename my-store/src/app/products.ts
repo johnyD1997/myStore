3852131eaa2402eb6b252d20dbff13eb16f960e4
Ваня Дубинников <ivan.dubinnikov@gmail.com>
2023-01-25 13:00:40 +0200

    Change ImagePath  at products.ts file; from ../assets(wrong path) to assets/...

--- my-store/src/app/products.ts
+++ my-store/src/app/products.ts
@@ -5,18 +5,18 @@ export const products = [
       name: 'Phone XL',
       price: 799,
       description: 'A large phone with one of the best screens',
-      ImagePath: '../assets/images/phone-screen-with-abstract-marble-aesthetic_53876-145553.jpeg'
+      ImagePath: 'assets/images/phone-screen-with-abstract-marble-aesthetic_53876-145553.jpeg'
     },
     {
       name: 'Phone Mini',
       price: 699,
       description: 'A great phone with one of the best cameras',
-      ImagePath: '../assets/images/1000958670-huawei-mate-xs-2-white-ohne-simlock-ohne-branding-003.jpeg'
+      ImagePath: 'assets/images/1000958670-huawei-mate-xs-2-white-ohne-simlock-ohne-branding-003.jpeg'
     },
     {
       name: 'Phone Standard',
       price: 299,
       description: '',
-      ImagePath: '../assets/images/iphone-14-pro-finish-select-202209-6-1inch-deeppurple.jpeg'
+      ImagePath: 'assets/images/iphone-14-pro-finish-select-202209-6-1inch-deeppurple.jpeg'
     }
   ];
