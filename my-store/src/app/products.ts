62e6e5681713902e4760bb8a3e0a76ff1d754ea1
Ваня Дубинников <ivan.dubinnikov@gmail.com>
2023-01-25 12:16:04 +0200

    Add images, buttons and functions to the shop

--- my-store/src/app/products.ts
+++ my-store/src/app/products.ts
@@ -1,17 +1,22 @@
+import { PathLocationStrategy } from "@angular/common";
+
 export const products = [
     {
       name: 'Phone XL',
       price: 799,
-      description: 'A large phone with one of the best screens'
+      description: 'A large phone with one of the best screens',
+      ImagePath: '../assets/images/phone-screen-with-abstract-marble-aesthetic_53876-145553.jpeg'
     },
     {
       name: 'Phone Mini',
       price: 699,
-      description: 'A great phone with one of the best cameras'
+      description: 'A great phone with one of the best cameras',
+      ImagePath: '../assets/images/1000958670-huawei-mate-xs-2-white-ohne-simlock-ohne-branding-003.jpeg'
     },
     {
       name: 'Phone Standard',
       price: 299,
-      description: ''
+      description: '',
+      ImagePath: '../assets/images/iphone-14-pro-finish-select-202209-6-1inch-deeppurple.jpeg'
     }
   ];
